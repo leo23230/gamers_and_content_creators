2f068f4b84a9a80269699a06fc53bbe70b1ad07c
leo23230 <54864620+leo23230@users.noreply.github.com>
2020-12-02 13:57:26 -0500

    oauth class

---
+++ lib/services/oath.dart
@@ -0,0 +1,7 @@
+class Oauth{
+  String clientId = "783751945612427336";
+  String clientSecret = "oFSUdhQ_6ytFTxP3PFZcsJs0PWSGB4bb";
+  String scope = "";
+  String redirectedURL = "";
+  String discordLoginUrl = "";
+}

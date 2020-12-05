6163415d4b6d9b2b89c6f2c9c88e3fc71050e74b
tomturrisi <Turrisitom@gmail.com>
2020-12-04 16:12:37 -0800

    start of twitch api

---
+++ lib/screens/authenticate/twich_sign_in.dart
@@ -0,0 +1,10 @@
+import 'package:gamers_and_content_creators/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:gamers_and_content_creators/services/oauth.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+const twitch = require('twitch.js');
+const client = new twitch.client();
+const token = 'qrhu5aoeaw353tf89u0m7dn7tvivzf';
+const PREFIX = '$';

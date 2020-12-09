532819ab28615f7b795c965fb0da113fe8693b60
leo23230 <54864620+leo23230@users.noreply.github.com>
2020-12-09 14:47:44 -0500

    Trying to receive queries

--- lib/main.dart
+++ lib/main.dart
@@ -39,5 +39,4 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,30 @@
 import 'package:gamers_and_content_creators/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gamers_and_content_creators/screens/home/profile_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+  int _currentIndex = 0;
+  final List <Widget> tabs = [
+    Text('Profile'),
+    Text('Home'),
+    Text('Matches')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-        backgroundColor: Colors.red[50],
+        backgroundColor: Colors.grey[700],
         appBar: AppBar(
-          title: Text('Brew Crew'),
-          backgroundColor: Colors.red[400],
+          title: tabs[_currentIndex],
+          backgroundColor: Colors.pink[500],
           elevation: 0.0,
           actions: <Widget>[
             FlatButton.icon(
@@ -21,6 +33,31 @@ class Home extends StatelessWidget {
               onPressed: () async {
                 await _auth.signOut();
               },
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.deepOrange[400],
+          unselectedItemColor: Colors.grey[600],
+          backgroundColor: Colors.grey[800],
+          onTap: (index) {
+            setState((){
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.forum),
+              title: Text('Matches'),
             ),
           ],
         ),
